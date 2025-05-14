89429830952bb41a59c4c7a73fb1579e34341e34
Feifei Lin <lin522mc@gmail.com>
2025-05-14 10:05:19 +0200

    add serial valve (see test_amf_serie)

--- test_amf_serie.py
+++ test_amf_serie.py
@@ -261,135 +261,252 @@ class Koelingsblok:
 
 
 class RVM:
-    def __init__(self, port = 'COM4'):
+    #### ALL COMMANDS
+    START_COMMAND = '/'
+    END_COMMAND = '\r'
+    POLLING_PERIOD = 0.05 ## time for repeated status check
+    ADDRESS = '1' #unit 1 -> command = f"/1Z\r" (check manual)
+    ROTATION_DELAY = 0.4  # 400 ms rotatietijd for Fast Mode
+
+    START_ANSWER = '/'
+    MASTER_ADDRESS = '0'
+    END_ANSWER = '\x03\r\n'
+
+    NO_R_REQUIRED = ['?', '!', 'H', 'T', 'Q','X','$','%','#','&','*']
+
+    ERROR_CODES = {'@': [0, 'No Error'],
+                   "`": [0, 'No Error'],
+                   'A': [1, 'Initialization'],
+                   'B': [2, 'Invalid command'],
+                   'C': [3, 'Invalid operand'],
+                   'D': [4, 'Missing trailing [R]'],
+                   'G': [7, 'Device not initialized'],
+                   'H': [8, 'Internal failure (valve)'],
+                   'I': [9, 'Plunger overload'],
+                   'J': [10, 'Valve overload'],
+                   'N': [14, 'A/D converter failure'],
+                   'O': [15, 'Command overflow'], }
+
+    STATUS_CODES = {'255': [255, 'Busy', 'Valve currently executing an instruction.'],
+                    '000': [0, 'Done', 'Valve available for next instruction.'],
+                    '128': [128, 'Unknown command', 'Check that the command is written properly'],
+                    '144': [144, 'Not homed', 'You forgot the homing! Otherwise, check that you have the right port configuration and try again.'],
+                    '145': [145, 'Move out of range', 'You’re probably trying to do a relative positioning and are too close to the limits.'],
+                    '146': [146, 'Speed out of range', 'Check the speed that you’re trying to go at.'],
+                    '224': [224, 'Blocked', 'Something prevented the valve to move.'],
+                    '225': [225, 'Sensor error', 'Unable to read position sensor. This probably means that the cable is disconnected.'],
+                    '226': [226, 'Missing main reference', ('Unable to find the valve’s main reference magnet '
+                                                            'during homing. This can mean that a reference magnet '
+                                                            'of the valve is bad/missing or that the motor is '
+                                                            'blocked during homing. Please also check motor '
+                                                            'cables and crimp.')],
+                    '227': [227, 'Missing reference', ('Unable to find a valve’s reference magnet during '
+                                                       'homing. Please check that you have the correct valve '
+                                                       'number configuration with command "/1?801". If '
+                                                       'not, change it according to the valve you are working '
+                                                       'with. This can also mean that a reference magnet of '
+                                                       'the valve is bad/missing or that the motor is blocked '
+                                                       'during homing.')],
+                    '228': [228, 'Bad reference polarity', ('One of the magnets of the reference valve has a bad '
+                                                            'polarity. Please check that you have the correct valve '
+                                                            'number configuration with command "/1?801". If '
+                                                            'not, change it according to the valve you are working '
+                                                            'with. This can also mean that a reference magnet has '
+                                                            'been assembled in the wrong orientation in the valve.')],
+                    }
+
+    # %%% COMMANDS
+    # %%%% Configuration Commands
+    SET_ADDRESS = '@ADDR='              # 1-9 or A-E, 1 by default
+    SET_ANSWER_MODE = '!50'             # Synchronous = 0, Asynchronous = 1, Asynchronous + counter = 2, 0 by default
+    SET_VALVE_CONFIGURATION = '!80'     # 4, 6, 8, 10 or 12 valve positions, 6 by default
+    RESET_VALVE_COUNTER = '!17'
+    SLOW_MODE = '-'                     # RVMFS only
+    FAST_MODE = '+'                     # RVMFS only
+    ACTIVATE_RS232 = '@RS232'           # Activated by default
+    ACTIVATE_RS485 = '@RS485F'
+    
+    # Plunger command
+    SET_PLUNGER_FORCE = '!30'           # 0,1,2 or 3 (0 = high and 3 = low force), 3 by default
+    SET_PEAK_SPEED = 'V'                # 0-1600 pulses/s, 150 by default
+    SET_ACCELERATION_RATE = 'L'         # 100-59'590 pulses/s², 1557 by default
+    SET_DECELERATION_RATE = 'l'         # 100-59'590 pulses/s², 59'590 by default
+    SET_SCALLING = 'N'                      # 0 or 1 (0 = 0.01 mm resolution, 1 = 0.00125mm resolution), 0 by default
+    
+    # %%%% Control Commands
+    EXECUTE = 'R'
+    REEXECUTE = 'X'
+    REPEAT = 'G'                        # 0-60'000, 0 by default = loop forever
+    REPEAT_SEQUENCE_START = 'g'
+    DELAY = 'M'                         # 0-86'400'000 milliseconds
+    HALT = 'H'                          # Pause the sequence AFTER finishng the current move
+    HARD_STOP = 'T'                     # Interrupt the current move and supress it from the sequence
+    POWER_OFF = '@POWEROFF'             # Shut down the pump
+
+    # %%%% Initialization Commands
+    HOME = 'Z'
+    HOME2 = 'Y'
+
+    # %%%% Valve Commands
+    SWITCH_SHORTEST_FORCE = 'B'
+    SWITCH_SHORTEST = 'b'
+
+    SWITCH_CLOCKWISE_FORCE = 'I'
+    SWITCH_CLOCKWISE = 'i'
+
+    SWITCH_COUNTERCLOCKWISE_FORCE = 'O'
+    SWITCH_COUNTERCLOCKWISE = 'o'
+    
+    # %%%% Plunger Commands
+    ABSOLUTE_POSITION = 'A'             # 0-3000 with N=0, 0-24'000 with N=1
+    ABSOLUTE_POSITION2 = 'a'            
+    
+    RELATIVE_PICKUP = 'P'               # 0-3000 with N=0, 0-24'000 with N=1
+    RELATIVE_PICKUP2 = 'p'
+    
+    RELATIVE_DISPENSE = 'D'             # 0-3000 with N=0, 0-24'000 with N=1
+    RELATIVE_DISPENSE2 = 'd'
+
+    # %%%% Report Commands
+    GET_STATUS = 'Q'
+    GET_PLUNGER_POSITION = '?'
+    GET_MAX_SPEED = '?2'
+    GET_PLUNGER_ACTUAL_POSITION = '?4'
+    GET_VALVE_POSITION = '?6'
+    GET_VALVE_NUMBER_MOVES = '?17'
+    GET_VALVE_NUMBER_MOVES_SINCE_LAST = '?18'
+    GET_SPEED_MODE = '?19' 
+    GET_FIRMWARE_CHECKSUM = '?20'
+    GET_FIRMWARE_VERSIOM = '?23'
+    GET_ACCELERATION = '?25'
+    GET_ADDRESS = '?26'
+    GET_DECELERATION = '?27'
+    GET_SCALLING = '?28'
+    GET_CONFIGURATION = '?76'
+    GET_PLUNGER_CURRENT = '?300'        # x10 mA
+    GET_ANSWER_MODE = '?500'
+    GET_NUMBER_VALVE_POSITION = '?801'
+    RESET = '$'
+    GET_SUPPLY_VOLTAGE = '*'            # x0.1 V
+    GET_UID = '?9000'
+    IS_PUMP_INITIALIZED = '?9010'       # 1 = True
+    GET_PUMP_STATUS_DETAILS = '?9100'
+    GET_STATUS_DETAILS = '?9200'
+    #####
+
+
+    def __init__(self, port='COM4', valve_ports=4, mode=0, address=1):
         self.port = port
+        self.valve_ports = valve_ports
+        self.mode = mode #the answer mode for mode = 0 the valve will response immediately 
+        self.address = address # unit 1 -> command = f"/1Z\r" (check manual))
         self.connected = False
         self.instrument = None
-        self.currentposition = 1 #home status
-        self.rotation_delay = 0.4  #the rotation time for 180 degree for RVMLP (1.5 s) and RVMFS (400 ms / 0.4s),
+        self.current_position = None
 
     def connect(self):
-        #Following should be 
         try:
-            product_list = amfTools.util.getProductList() # get the list of AMF products connected to the computer
-
-            product : amfTools.Device = None
-            self.instrument : amfTools.AMF = None
-            for product in product_list:
-                if "RVM" in product.deviceType:
-                    self.instrument = amfTools.AMF(product)
-                    break
-
-            if self.instrument is None:
-            # Try forced port connection if no RVM detected
-               self.instrument = amfTools.AMF(self.port)
-
-
-            self.instrument.connect() 
+            self.instrument = serial.Serial(self.port, baudrate=9600, timeout=1)
             self.connected = True
-            print("connection",self.connected)
-            self.initialize_valve()
-            return True
-    
-        except Exception as err:
+            print(f"RVM Industrial Microfluidic Rotary Valve is in position {self.port}")
+
+            # Basisconfiguratie
+            self.send_command(self.SET_ADDRESS, self.address)
+            self.send_command(self.SET_ANSWER_MODE, self.mode)
+            self.send_command(self.SET_VALVE_CONFIGURATION, self.valve_ports)
+
+            # Valve initialiseren
+            self.home()
+            self.switch_position(1)
+
+        except serial.SerialException as err:
             messagebox.showerror("Error",
-                    f"An error occurred while connecting RVM Industrial Microfluidic Rotary Valve: {err}")
+                    f"An error occurred while connecting RVM Industrial Microfluidic Rotary Valve: {err}") 
             self.connected = False
-            return False
-        
-       ##SIMULATION the following is used only for simulation
-        # self.connected = True
-        # return self.connected
-        ##
 
     def disconnect(self):
-        if self.connected and self.instrument:
-            try:
-                self.instrument.disconnect()
-                print("RVM disconnected.")
-            except Exception as err: 
-                messagebox.showerror("Error",
-                    f"An error occurred while disconnecting RVM Industrial Microfluidic Rotary Valve: {err}")
-        self.connected = False
-
-    #home status 
-    def initialize_valve(self): 
-        if not self.connected:
-            raise ConnectionError("RVM Industrial Microfluidic Rotary Valve is not connected.")
-        
-        # Check if the product is homed (if not, home it)
-        try:
-            if not self.instrument.getHomeStatus(): 
-                self.instrument.home()
-                time.sleep(self.rotation_delay)  # Give time for homing
-
-            else:
-                print("RVM Industrial Microfluidic Rotary Valve is already homed.")
-
-            # Always move to position 1 after homing (default start position)
-            self.instrument.valveShortestPath(1)
-            time.sleep(self.rotation_delay) #give time for rotation
-            self.currentposition = 1
-            print("RVM Industrial Microfluidic Rotary Valve moved to position 1/ON State.")
-
-        except Exception as err:
-            messagebox.showerror("Error",
-                 f"An error occurred while initializing the RVM Industrial Microfluidic Rotary Valve : {err}")
-            
-    
-    def set_valve(self, position: int):  
         if self.connected:
-            if position != 1 and position != 2:
-                messagebox.showerror("Error",
-                    f"The position of the RVM Industrial Microfluidic Rotary Valve can only be 1 (ON) or 2 (OFF), but received: {position}"
-                )
-                return False
-        else:
-            messagebox.showerror("Error", "RVM Industrial Microfluidic Rotary Valve  is not connected.")
-            return False #Operation failed
+            self.instrument.close()
+            self.connected = False
+            print("RVM Industrial Microfluidic Rotary Valve is disconnected")
+
+    def home(self):
+        print("Homing RVM Industrial Microfluidic Rotary Valve...")
+        self.send_command(self.HOME)
+        time.sleep(self.ROTATION_DELAY)
+        self.check_status()
+        print("RVM Industrial Microfluidic Rotary Valve homed")
+
+    def switch_position(self, position: int):
+        if position not in [1, 2]:
+            raise ValueError("The position of the valve can only be 1 or 2")
+
+        print(f"Switching RVM Industrial Microfluidic Rotary Valve to position {position}")
+        self.send_command(self.SWITCH_SHORTEST, position)
+        time.sleep(self.ROTATION_DELAY)
+        self.check_status()
+        self.current_position = position
+        print(f"RVM Industrial Microfluidic Rotary Valve is in positie {position}.")
+
+    def get_position(self):
+        pos = self.send_command(self.GET_VALVE_POSITION)
+        self.current_position = int(pos)
+        print(f"The current position of RVM Industrial Microfluidic Rotary Valve is {self.current_position}")
+        return self.current_position
+
+    def check_status(self):
+        while True:
+            status = self.send_command(self.GET_STATUS_DETAILS)
+            if status == '0':
+                break
+            elif status == '255':
+                time.sleep(self.POLLING_PERIOD)
+            else:
+                raise ValveError(f"invalid status: {status}")
+
+    def build_command(self, command, parameter=None):
+        cmd = f"{self.START_COMMAND}{self.ADDRESS}{command}"
+        if parameter is not None:
+            cmd += str(parameter)
+        if command[0] not in self.NO_R_REQUIRED:
+            cmd += self.EXECUTE
+        cmd += self.END_COMMAND
+        return cmd
+
+    def send_command(self, command, parameter=None):
+        if not self.connected:
+            raise ValveError("RVM Industrial Microfluidic Rotary Valve is not connected")
+
+        full_cmd = self.build_command(command, parameter)
+        self.instrument.reset_input_buffer()
+        self.instrument.write(full_cmd.encode())
+        error, data = self.read_response()
+        self.check_error(error)
+        return data
+
+    def read_response(self):
+        response = self.instrument.read_until(self.END_ANSWER.encode()).decode('utf-8').strip()
+        if not response.startswith('/'):
+            raise ValveError(f"Ongeldige respons: {response}")
+
+        response = response[2:]  # Verwijder '/' en adres
+        error_code = response[0]
+        data = response[1:].replace(self.END_ANSWER.strip(), '')
+        error = {'@': [0, 'Geen fout'], '`': [0, 'Geen fout']}.get(error_code, [999, 'Onbekende fout'])
+        return error, data
+
+    def check_error(self, error):
+        if error[0] != 0:
+            raise ValveError(f"Fout {error[0]}: {error[1]}")
         
-        ## check if is in position 1 then move to postion 2 otherwise give a warning
-        if self.currentposition == 1:
-            if position == 2:
-                # Move to position 2
-                try:
-                    ##VOOR SIMULATION UITZETTEN
-                    self.instrument.valveShortestPath(2)
-                    time.sleep(self.rotation_delay)
-                    ###
-                    
-                    self.currentposition = 2
-                    print(f"RVM Industrial Microfluidic Rotary Valve moved to position 2/OFF state.")
-                    return True
-                except Exception as err:
-                    messagebox.showerror("Error",
-                        f"An error occurred while moving to position 2/OFF state : {err}")
-            elif position == 1:
-                print(f"RVM Industrial Microfluidic Rotary Valve is already at position {self.currentposition}/OFF state")
-                return False
-            else:
-                print(f"Invalid position: {position}")
-                # return False
-           
-        elif self.currentposition == 2:
-            if position == 1:
-                # Move to position 1
-                try:
-                    ## VOOR SIMULATION UITZETTEN
-                    self.instrument.valveShortestPath(1)
-                    time.sleep(self.rotation_delay)
-                    ###
-                    
-                    self.currentposition = 1
-                    print(f"RVM Industrial Microfluidic Rotary Valve moved to position 1/ON state")
-                    return True
-                except Exception as err:
-                   messagebox.showerror("Error",
-                        f"An error occurred while moving to position 1/ON tate : {err}")
-            elif position == 2:
-                print(f"RVM Industrial Microfluidic Rotary Valve is already at position {self.currentposition}/ON state")
-            else:
-                print(f"Invalid position: {position}")
+#for rvm
+class ValveError(Exception):
+    '''Error class for AMF modules.
+
+        Attributes:
+          message -- explanation of the error
+    '''
+
 
 
     
@@ -815,11 +932,11 @@ class AutomatedSystemUI:
         
     def set_valve(self):
         position = self.valve_pos_var.get()
-        if self.valve.set_valve(position):
+        if self.valve.switch_position(position):
             self.update_valve()
 
     def update_valve(self):
-        target_position = self.valve.currentposition
+        target_position = self.valve.get_position
         self.update_run_var()
         if target_position is not None:
             if self.currentposition != target_position:                
