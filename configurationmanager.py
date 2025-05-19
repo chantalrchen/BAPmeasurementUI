18cd63b4c92e46612d67c54027e3ec529de0d73a
Chantal <chantalrhchen@hotmail.com>
2025-05-19 17:58:40 +0200

    configuration manager

---
+++ configurationmanager.py
@@ -0,0 +1,44 @@
+import json
+import os
+
+class ConfigManager:
+    def __init__(self, base_dir=".", filename="config.json"):
+        self.config_path = os.path.join(base_dir, filename)
+        self.config = {}
+
+        # Ensure that folder exists
+        config_folder = os.path.dirname(self.config_path)
+        if config_folder and not os.path.exists(config_folder):
+            os.makedirs(config_folder)
+            
+        # Load the configuration
+        self.load_config()
+
+    def load_config(self):
+        if os.path.exists(self.config_path):
+            with open(self.config_path, 'r') as openfile:
+                self.config = json.load(openfile)
+                return self.config
+        else:
+            self.config = {}
+            return self.config
+
+    def save_config(self):
+        with open(self.config_path, 'w') as outfile:
+            json.dump(self.config, outfile, indent=6)
+            return True
+
+    def get_com_ports(self):
+        #If com_ports doesn't exist, then it returns {}
+        return self.config.get("com_ports", {})
+
+    def set_com_ports(self, ports):
+        # Store the com values under "com_ports"
+        self.config["com_ports"] = ports
+        self.save_config()
+    
+    def update_com_port(self, device_name, port):
+        if "com_ports" not in self.config:
+            self.config["com_ports"] = {}
+        self.config["com_ports"][device_name] = port
+        self.save_config()
