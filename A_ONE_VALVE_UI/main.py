e1eaffe6f19950769f0b892f697b09108b61b0a4
chantalrchen <125665895+chantalrchen@users.noreply.github.com>
2025-06-11 00:27:46 +0200

    Add files via upload

--- A_ONE_VALVE_UI/main.py
+++ A_ONE_VALVE_UI/main.py
@@ -1,15 +1,26 @@
 import tkinter as tk
 from uiscrollable import AutomatedSystemUI
-# from uiscrollable_2valves import AutomatedSystemUI
+import tkinter.messagebox
+
+##To use this application
+# install the following packages: pip install bronkhorst-propar, pyserial, matplotlib 
+
+def on_closing():
+    """ Messagebox to ask the user whether they want to close the app before exiting it to prevent accident closure 
+
+    Reference: https://stackoverflow.com/questions/26168967/invalid-command-name-while-executing-after-script
+    """
+    response= tkinter.messagebox.askyesno('Exit','Are you sure you want to exit?')
+    if response:
+        root.withdraw()
+        root.quit()
+        
 if __name__ == "__main__":
     root = tk.Tk()
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     
+    #When opening the UI ensuring it is full screen
     root.state("zoomed")
     
     app = AutomatedSystemUI(root)
     root.mainloop()
-
-    
-##VRAGEN
-##ALS VALVE POSITION OP UIT STAAT, MOET HET DAN OOK NOG CONCENTRATIE GEVEN?
-    
